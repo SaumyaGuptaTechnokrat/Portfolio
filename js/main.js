9d92a7294819ea905d3a633a5d54c0a7bc39aca8
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-08-10 18:31:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,3 +22,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+$(document).ready(function(){
+	$("#icons").hover(function(){
+		$(this).addClass("fa-bounce");
+	});
+});
