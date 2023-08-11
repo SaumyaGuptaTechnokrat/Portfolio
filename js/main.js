48b9ad4d5abf240bce6e5431e6fbc1a0323e734c
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-08-11 12:15:41 +0530

    OnMouse enter and leave bounce
    OnMouse enter and leave bounce

--- js/main.js
+++ js/main.js
@@ -22,8 +22,4 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-$(document).ready(function(){
-	$("#icons").hover(function(){
-		$(this).addClass("fa-bounce");
-	});
-});
+
